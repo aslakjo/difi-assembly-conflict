dd906f3eccbde3efd0aefe52cfe25f7c766911cf
sukchr <sukchr>
2017-11-15 13:28:50 +0100

    Intial setup with difi-oppslagstjeneste-klient 6.0.1.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                Difi.Oppslagstjeneste.Klient.Miljø.FunksjoneltTestmiljøVerifikasjon1
+            );
+        }
+    }
+}
